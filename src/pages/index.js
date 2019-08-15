50cda19c9430ffecc43f55927720e1f0d4ba6b37
Brady Dowling <brady@amazon.com>
2019-08-15 08:34:08 -0400

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>My WordPress Blog</h1>
+    <h4>Posts</h4>
+    {data.allWordpressPost.edges.map(({ node }) => (
+      <div>
+        <p>{node.title}</p>
+        <Link to={node.slug}>
+          <p>{node.title}</p>
+        </Link>
+        <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      </div>
+    ))}
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allWordpressPost(sort: { fields: [date], order: DESC }) {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
